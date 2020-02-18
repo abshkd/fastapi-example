2f387944f6e8c1fc2d5144672c8f85bf6008e1e2
Abhishek Dujari <abhishek.dujari@gmail.com>
2020-02-18 22:33:45 +0700

    Add a slow endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import time
+import random
 
 app = FastAPI()
 
+class Game(BaseModel):
+    name: str
+    player: str
+    level: int = 1
+    score: int = 0
 
 @app.get("/")
 def read_root():
@@ -10,4 +19,15 @@ def read_root():
 
 @app.get("/game/new/{player_name}")
 def game_new(player_name: str = "player1"):
-    return {"game": "42", "player": player_name, "message" : "New game started"}
+    return {"game": "42", "player": player_name, "message": "Welcome %s New game started.".format(player_name)}
+
+
+@app.get("/game/resume/{player_name}")
+def game_resume(player_name: str):
+    return {"game": "42", "player": player_name}
+
+
+@app.post("/game/play")
+async def game_play(game: Game):
+    time.sleep(12 + (random.randrange(5,60)))
+    return game
